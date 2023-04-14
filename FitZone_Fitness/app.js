de0a9a8b7ae6c7a9eb88547cdb501141ff6a63b6
Shivansh Bhardwaj <ipsshivansh@gmail.com>
2023-04-14 18:29:13 +0530

    adding the validation module to the sigh up page

--- FitZone_Fitness/app.js
+++ FitZone_Fitness/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const emailValidator = require('email-validator'); //to validate the email using a npm module
 
 const app = express();
 
@@ -40,4 +41,38 @@ mongoose.connect(db_link)
 })
 .catch(function(err){
     console.log(err);
+});
+
+const userSchema=mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true,
+        validate:function(){
+            return emailValidator.validate(this.email);
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:8
+    },
+    confirmPassword:{
+        type:String,
+        require:true,
+        minlength:8,
+        validate:function(){
+            return this.confirmPassword==this.password
+        }
+    }
+
+});
+//Aplying hooks so that the repeated data ,confirm password is not saved in db
+userSchema.pre('save',function(){
+    this.confirmPassword=undefined;
+
 });
